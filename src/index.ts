3964114bcf4cf6713c85d58e352206ae1165e66d
Mihail Kirov <mihail.kirov@limechain.tech>
2024-01-21 15:29:33 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -40,37 +40,31 @@ async function sendRawTransaction() {
     loadTrustedSetup(path.resolve("src/trusted_setup.txt"))
     const commitment = blobToKzgCommitment(dummyBlob);
     const proof = computeBlobKzgProof(dummyBlob, commitment);
-
     const isValid = verifyBlobKzgProof(dummyBlob, commitment, proof)
+
     console.log(isValid)
-    console.log(ethers.utils.sha256(commitment))
     console.log( "0x01" + ethers.utils.sha256(commitment).substr(4, 64)) // versioned hash
+    
     const txPayloadBody = {
         chainId: ethers.utils.hexlify(chainId),
         nonce: ethers.utils.hexlify(nonce),
         maxPriorityFeePerGas: ethers.utils.hexlify(feeData.maxPriorityFeePerGas),
         maxFeePerGas: ethers.utils.hexlify(feeData.maxFeePerGas),
         gasLimit: ethers.utils.hexlify(250000),
-        // gasPrice: ethers.utils.hexlify(feeData.gasPrice),
         to,
-        value: "0x00",
-        data: "0x00",
-        accessList: "0x00",
+        value: "0x",
+        data: "0x",
+        accessList: [],
         maxFeePerBlobGas: ethers.utils.hexlify(10000000),
-        blobVersionedHashes: "0x00" + ethers.utils.sha256(commitment).substr(2, 64)
+        blobVersionedHashes: ["0x01" + ethers.utils.sha256(commitment).substr(4, 64)]
     };
-
     console.log(txPayloadBody);
 
-    let encodedPayload: Array<string> = [];
-    Object.values(txPayloadBody).forEach((value: any) => encodedPayload.push(ethers.utils.RLP.encode(ethers.utils.arrayify(value))));
+    const rlpEncodedPayload = ethers.utils.RLP.encode(Object.values(txPayloadBody))
+    console.log(rlpEncodedPayload)
 
-    // BLOB_TX_TYPE concatenated with encoded payload
     const BLOB_TX_TYPE = '0x03'; // source https://github.com/ethereum/EIPs/blob/master/EIPS/eip-4844.md
-    
-    // encodedPayload = ethers.utils.arrayify(BLOB_TX_TYPE).concat(encodedPayload);
-    // console.log(encodedPayload)
-    const payloadForHashing = ethers.utils.concat([BLOB_TX_TYPE, ...encodedPayload]);
+    const payloadForHashing = BLOB_TX_TYPE + rlpEncodedPayload.slice(2)
     console.log(payloadForHashing)
 
     // Calculate the digest
@@ -80,21 +74,19 @@ async function sendRawTransaction() {
 
     let signedTransaction = {
         ...txPayloadBody,
-        v,
+        y_parity: "0x0" + (v - 27),
         r: ethers.utils.hexZeroPad(r, 32),
         s: ethers.utils.hexZeroPad(s, 32),
     }
     console.log(signedTransaction)
 
-    // console.log(ethers.utils.hexlify(signedTransaction))
-    let arr: any[] = [];
-    Object.values(signedTransaction).forEach(v => { arr = ethers.utils.concat([...arr, ...ethers.utils.arrayify(v)]) as any});
-    let hexlifiedSignedTransaction = ethers.utils.hexlify(arr)
-    console.log(hexlifiedSignedTransaction);
-    const txHash = await provider.send('eth_sendRawTransaction', [hexlifiedSignedTransaction]);
-    console.log(txHash)
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-4844.md#networking
+    const networkRepresentation = ethers.utils.RLP.encode([Object.values(signedTransaction), [dummyBlob], [commitment], [proof]])
+    console.log(networkRepresentation.length)
+
+    const txHash = await provider.send('eth_sendRawTransaction', [networkRepresentation]);
+    return txHash;
 }
 
 sendRawTransaction()
-    .then(txHash => console.log('Transaction Hash:', txHash))
     .catch(error => console.error(error));
