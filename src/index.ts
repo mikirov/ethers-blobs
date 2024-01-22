30a83376c69d3be54f9426fa018f671440e04dfa
Mihail Kirov <mihail.kirov@limechain.tech>
2024-01-22 17:42:47 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { ethers } from "ethers";
 import { assert } from "chai";
+import { ethers } from "ethers";
+
 import * as dotenv from "dotenv";
+import * as http from 'http'
+import * as path from 'path';
+
+import { Chain, Common, Hardfork } from '@ethereumjs/common'
+import { BlobEIP4844Transaction } from '@ethereumjs/tx'
 
 import pkg from 'c-kzg';
-import * as path from 'path';
+import axios from 'axios';
 
 const {
     blobToKzgCommitment,
@@ -11,14 +17,16 @@ const {
     verifyBlobKzgProof,
     loadTrustedSetup
   } = pkg;
-  
+
+const common = new Common({ chain: Chain.Goerli, hardfork: Hardfork.Cancun, eips: [4844] })
+
 dotenv.config();
 
 assert(process.env.OWNER_PK !== undefined);
 assert(process.env.RPC_URL !== undefined);
 
-const ownerPrivateKey: string = process.env.OWNER_PK;
-const rpcUrl: string = process.env.RPC_URL;
+const ownerPrivateKey = process.env.OWNER_PK;
+const rpcUrl = process.env.RPC_URL;
 
 // Setup provider, wallet
 const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
@@ -27,66 +35,61 @@ const signer: ethers.Wallet = new ethers.Wallet(ownerPrivateKey, provider);
 async function sendRawTransaction() {
     const from: string = await signer.getAddress();
     const to: string = await ethers.Wallet.createRandom().getAddress();
-    const nonce: number = await provider.getTransactionCount(from);
+    const nonce: number = await provider.getTransactionCount(from, 'pending');
     const chainId: number = await signer.getChainId();
     const feeData: ethers.providers.FeeData = await provider.getFeeData();
+    // console.log(feeData)
 
-    // console.log(feeData);
-
-    // blob generation
     const BYTES_PER_BLOB = 4096 * 32
-    let dummyBlob = Buffer.alloc(BYTES_PER_BLOB, "*")
+    const dummyBlob = Buffer.alloc(BYTES_PER_BLOB, "*")
 
     loadTrustedSetup(path.resolve("src/trusted_setup.txt"))
     const commitment = blobToKzgCommitment(dummyBlob);
     const proof = computeBlobKzgProof(dummyBlob, commitment);
     const isValid = verifyBlobKzgProof(dummyBlob, commitment, proof)
 
-    console.log(isValid)
-    console.log( "0x01" + ethers.utils.sha256(commitment).substr(4, 64)) // versioned hash
+    // console.log(isValid)
+    // console.log( "0x01" + ethers.utils.sha256(commitment).substr(4, 64)) // versioned hash
     
-    const txPayloadBody = {
+    const txData = {
         chainId: ethers.utils.hexlify(chainId),
         nonce: ethers.utils.hexlify(nonce),
+        // nonce: ethers.utils.hexlify(nonce),
         maxPriorityFeePerGas: ethers.utils.hexlify(feeData.maxPriorityFeePerGas),
         maxFeePerGas: ethers.utils.hexlify(feeData.maxFeePerGas),
-        gasLimit: ethers.utils.hexlify(250000),
+        maxFeePerBlobGas: '0xffff',
+        gasLimit: "0x989680", // 10 million
         to,
-        value: "0x",
+        value: "0x00",
         data: "0x",
+        type: '0x03',
         accessList: [],
-        maxFeePerBlobGas: ethers.utils.hexlify(10000000),
-        blobVersionedHashes: ["0x01" + ethers.utils.sha256(commitment).substr(4, 64)]
+        // maxFeePerBlobGas: ethers.utils.arrayify(ethers.utils.hexlify(159000000000)),
+        blobVersionedHashes: ["0x01" + ethers.utils.sha256(commitment).substr(4, 64)],
+        kzgCommitments: [ethers.utils.hexlify(commitment)],
+        blobs: [dummyBlob],
+        kzgProofs: [ethers.utils.hexlify(proof)]
     };
-    console.log(txPayloadBody);
 
-    const rlpEncodedPayload = ethers.utils.RLP.encode(Object.values(txPayloadBody))
-    console.log(rlpEncodedPayload)
+    const tx = BlobEIP4844Transaction.fromTxData(txData, { common })    
+    const signedTx = tx.sign(Buffer.from(ownerPrivateKey, 'hex'))
 
-    const BLOB_TX_TYPE = '0x03'; // source https://github.com/ethereum/EIPs/blob/master/EIPS/eip-4844.md
-    const payloadForHashing = BLOB_TX_TYPE + rlpEncodedPayload.slice(2)
-    console.log(payloadForHashing)
+    console.log(signedTx.serializeNetworkWrapper())
 
-    // Calculate the digest
-    const digest = ethers.utils.keccak256(payloadForHashing);
-    const signature = await signer.signMessage(ethers.utils.arrayify(digest));
-    const { r, s, v } = ethers.utils.splitSignature(signature);
+    const rawTx = ethers.utils.hexlify(signedTx.serializeNetworkWrapper())
+    const data = JSON.stringify(
+        { "jsonrpc": "2.0", "method": "eth_sendRawTransaction", "params": [rawTx], "id": 1 }
+    )
+    
+    const res = await axios({
+        method: 'post',
+        url: process.env.RPC_URL,
+        data
+    })
 
-    let signedTransaction = {
-        ...txPayloadBody,
-        y_parity: "0x0" + (v - 27),
-        r: ethers.utils.hexZeroPad(r, 32),
-        s: ethers.utils.hexZeroPad(s, 32),
-    }
-    console.log(signedTransaction)
-
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-4844.md#networking
-    const networkRepresentation = ethers.utils.RLP.encode([Object.values(signedTransaction), [dummyBlob], [commitment], [proof]])
-    console.log(networkRepresentation.length)
-
-    const txHash = await provider.send('eth_sendRawTransaction', [networkRepresentation]);
-    return txHash;
+    return res;
 }
 
 sendRawTransaction()
+    .then(res => console.log(res))
     .catch(error => console.error(error));
